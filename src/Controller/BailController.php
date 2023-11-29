69b1d64c2aacc2896ea03e4fbf4559ac430cbe05
DJAE <djaeardi80@gmail.com>
2023-11-29 12:02:37 +0100

    Ajout function quittancePDF

--- src/Controller/BailController.php
+++ src/Controller/BailController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Bail;
+use App\Entity\Paiement;
+use Dompdf\Dompdf;
 
 class BailController extends AbstractController
 {
@@ -38,5 +40,42 @@ class BailController extends AbstractController
         return $this->render('bail/lister.html.twig', [
             'pBails' => $bails,]);
 
+    }
+
+    // Générer la quittance sous forme PDF pour un bail donné
+    public function quittancePDF(ManagerRegistry $doctrine, int $id){
+
+        $bail = $doctrine->getRepository(Bail::class)->find($id);
+        $repositoryPaiement = $doctrine->getRepository(Paiement::class);
+        $paiements= $repositoryPaiement->findAll();
+
+        if (!$bail) {
+            throw $this->createNotFoundException(
+            'Aucun bail trouvé'
+            );
+        }
+
+        // Instanciation de la librairie DOMPDF
+        $dompdf = new Dompdf();
+
+        // Contenu HTML
+        $html = $this->renderView('bail/quittancePDF.html.twig', [
+            'bail' => $bail,
+            'pPaiements' => $paiements,
+        ]);
+
+        // Chargement du contenu HTML
+        $dompdf->loadHtml($html);
+
+        // Configuration des options
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Rendu du PDF
+        $dompdf->render();
+
+        // Envoi du PDF dans la réponse
+        $dompdf->stream("bail_".$bail->getId().".pdf", [
+            "Attachment" => false
+        ]);
     }
 }
