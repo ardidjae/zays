b80d953604b36ca6e4de54d39db4c6cc94a03ba5
DJAE <djaeardi80@gmail.com>
2023-12-11 17:35:34 +0100

    BailCloturerType

--- src/Form/BailCloturerType.php
+++ src/Form/BailCloturerType.php
@@ -21,29 +21,59 @@ class BailCloturerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateDebut')
-            ->add('MontantHC')
-            ->add('MontantCharges')
-            ->add('MontantCaution')
-            ->add('NomCaution1')
-            ->add('NomCaution2')
-            ->add('DateFin')
-            ->add('DureeBail')
-            ->add('BailSigne')
-            ->add('EtatLieuEntree')
-            ->add('EtatLieuSortie')
-            ->add('AttestationAssurance')
-            ->add('MontantPremEcheance')
-            ->add('MontantDerEcheance')
-            ->add('TrimestreReference')
-            ->add('PieceJustificative')
-            ->add('archive')
-            ->add('CautionRestituer', TextType::class, [
-                'label' => 'Montant caution à restituer :',
-            ])
-            ->add('appartement')
-            ->add('associe')
-            ->add('locataires')
+        ->add('dateDebut', DateType::class, [
+            'widget' => 'single_text',
+            'format' => 'yyyy-MM-dd',
+            'disabled' => true,
+        ])
+        ->add('MontantHC', TextType::class, [
+            'label' => 'Montant HC :',
+            'disabled' => true,
+        ])
+        ->add('MontantCharges', TextType::class, [
+            'label' => 'Montant des charges :',
+            'disabled' => true,
+        ])
+        ->add('MontantCaution', TextType::class, [
+            'label' => 'Montant de la caution :',
+            'disabled' => true,
+        ])
+        ->add('MontantPremEcheance', TextType::class, [
+            'label' => 'Montant Premiere Echeance :',
+            'disabled' => true,
+        ])
+        ->add('MontantDerEcheance', TextType::class, [
+            'label' => 'Montant Derniere Echeance :',
+        ])
+        ->add('DateFin', DateType::class, [
+            'widget' => 'single_text',
+            'format' => 'yyyy-MM-dd',
+            'label' => 'Date de fin du bail :',
+        ])
+        ->add('EtatLieuEntree', TextType::class, [
+            'label' => 'État des lieux à l\'entrée :',
+        ])
+        ->add('EtatLieuSortie', TextType::class, [
+            'label' => 'État des lieux à la sortie :',
+        ])
+        ->add('appartement', EntityType::class, [
+            'class' => 'App\Entity\Appartement',
+            'choice_label' => 'porte',
+            'label' => 'Appartement :',
+            'disabled' => true,
+        ])
+        ->add('locataires', CollectionType::class, ['entry_type' => LocataireType::class,
+            'entry_options' => ['label' => false],
+            'allow_add' => true,
+            'disabled' => true,
+        ])
+        ->add('Archive', TextType::class, [
+            'label' => 'Archive',
+        ])
+        ->add('CautionRestituer', TextType::class, [
+            'label' => 'Montant caution à restituer :',
+        ])
+        ->add('enregistrer', SubmitType::class, array('label' => 'Modifier Bail'))
         ;
     }
 
