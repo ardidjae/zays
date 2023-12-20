f06a9912c3d02c4d28864b0b10c2bcc3ec5166ea
DJAE <djaeardi80@gmail.com>
2023-12-20 11:52:31 +0100

    CategorieType

---
+++ src/Form/CategorieType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle', TextType::class, [
+                'label' => 'Libelle',
+            ])
+
+            ->add('enregistrer', SubmitType::class, array('label' => 'Nouvel Categorie'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
