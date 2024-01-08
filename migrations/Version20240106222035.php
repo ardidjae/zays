ea7ffb24c10bf4f4d7e02cb57481d7a61d6df931
DJAE <djaeardi80@gmail.com>
2024-01-08 15:29:27 +0100

    version Locataire

---
+++ migrations/Version20240106222035.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240106222035 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE locataire ADD email VARCHAR(255) NOT NULL, ADD telephone DOUBLE PRECISION NOT NULL, ADD piece_justificative VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
