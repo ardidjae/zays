d797def333d4df936124cb291cb27f8a0ff3aeda
DJAE <djaeardi80@gmail.com>
2024-01-12 15:29:57 +0100

    ajout accès par role à la liste

--- src/Controller/MouvementController.php
+++ src/Controller/MouvementController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Mouvement;
 use App\Entity\SousCategorie;
 use App\Form\MouvementModifierType;
+use Symfony\Component\Security\Core\Security;
 
 class MouvementController extends AbstractController
 {
@@ -21,20 +22,38 @@ class MouvementController extends AbstractController
         ]);
     }
 
-    public function listerMouvement(ManagerRegistry $doctrine){
+    public function listerMouvement(ManagerRegistry $doctrine, Security $security){
+
+        // Vérifier si l'utilisateur est connecté et a le rôle ROLE_ASSOCIE
+        if ($security->isGranted('IS_AUTHENTICATED_FULLY') && $security->isGranted('ROLE_ASSOCIE')) {
+            // Récupérer l'utilisateur connecté
+            $user = $this->getUser();
 
         $repository = $doctrine->getRepository(Mouvement::class);
         $sousCategoriesRepository = $doctrine->getRepository(SousCategorie::class);
 
         $sousCategories = $sousCategoriesRepository->findAll();
 
-        // Récupérer tous les mouvements avec une sous-catégorie null
-        $mouvements = $repository->findBy(['souscategorie' => null]);
+       // Vérifier le rôle de l'utilisateur
+        if ($security->isGranted('ROLE_ASSOCIE')) {
+            // Si l'utilisateur a le rôle ROLE_ASSOCIE, afficher tous les mouvements
+            $mouvements = $repository->findAll();
+        } elseif ($security->isGranted('ROLE_ADMIN')) {
+            // Si l'utilisateur a le rôle ROLE_ADMIN, afficher les mouvements avec sous-catégorie null
+            $mouvements = $repository->findBy(['souscategorie' => null]);
+        } else {
+            // Rediriger vers la page de connexion si l'utilisateur n'a ni ROLE_ASSOCIE ni ROLE_ADMIN
+            return $this->redirectToRoute('app_login');
+        }
 
         return $this->render('mouvement/lister.html.twig', [
             'pMouvements' => $mouvements,
             'pSousCategories' => $sousCategories,
         ]);
+        } else {
+            // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté ou n'a pas le rôle associé
+            return $this->redirectToRoute('app_login');
+        }
 
     }
 
