968016fbff5d2bf98277cfa65abaed5d80bc8215
DJAE <djaeardi80@gmail.com>
2024-01-25 16:26:38 +0100

    LocataireType nouveau champs pieceJustificative

--- src/Form/LocataireType.php
+++ src/Form/LocataireType.php
@@ -15,6 +15,9 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class LocataireType extends AbstractType
 {
@@ -41,6 +44,30 @@ class LocataireType extends AbstractType
                 'label' => 'Lieu de Naissance',
             ])
             //->add('bails')
+            ->add('pieceJustificative', FileType::class, [
+                'label' => 'Piece Justificative (PDF file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('enregistrer', SubmitType::class, array('label' => 'Nouveau Locataire'))
 
         ;
     }
