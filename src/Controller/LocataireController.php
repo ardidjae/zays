0fcb5f9e4052d35428d720191b56b0d3ec8043a6
DJAE <djaeardi80@gmail.com>
2024-01-25 16:21:37 +0100

    upload file locataire

--- src/Controller/LocataireController.php
+++ src/Controller/LocataireController.php
@@ -9,6 +9,9 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Locataire;
 use App\Form\LocataireType;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 
 class LocataireController extends AbstractController
 {
@@ -20,27 +23,50 @@ class LocataireController extends AbstractController
         ]);
     }
 
-    public function ajouterLocataire(ManagerRegistry $doctrine,Request $request){
+    public function ajouterLocataire(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger){
         $locataire = new Locataire();
-
         $form = $this->createForm(LocataireType::class, $locataire);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
-
-                $locataire = $form->getData();
-                $locataire->setArchive(0);
-                $entityManager = $doctrine->getManager();
-                $entityManager->persist($locataire);
-                $entityManager->flush();
-
-            return $this->render('locataire/consulter.html.twig', ['locataire' => $locataire,]);
+            /** @var UploadedFile $pieceJustificativeFile */
+            $pieceJustificativeFile = $form->get('pieceJustificative')->getData();
+    
+            // this condition is needed because the 'pieceJustificative' field is not required
+            // so the file must be processed only when a file is uploaded
+            if ($pieceJustificativeFile) {
+                $originalFilename = pathinfo($pieceJustificativeFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$pieceJustificativeFile->guessExtension();
+    
+                // Move the file to the directory where pieces are stored
+                try {
+                    $pieceJustificativeFile->move(
+                        $this->getParameter('pieces_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+    
+                // updates the 'pieceJustificative' property to store the file name
+                // instead of its contents
+                $locataire->setPieceJustificative($newFilename);
+            }
+    
+            // persist the $locataire variable or any other work
+            $locataire->setArchive(0);
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($locataire);
+            $entityManager->flush();
+    
+            return $this->render('locataire/consulter.html.twig', ['locataire' => $locataire]);
+        } else {
+            return $this->render('locataire/ajouter.html.twig', ['form' => $form->createView()]);
         }
-        else
-        {
-            return $this->render('locataire/ajouter.html.twig', array('form' => $form->createView(),));
-	    }
     }
+    
 
     public function consulterLocataire(ManagerRegistry $doctrine, int $id){
 
